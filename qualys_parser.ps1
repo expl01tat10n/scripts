2a095e2db33a54521cc1a8935009a93e52b3e373
expl01tat10n <dewoodmail@gmail.com>
2016-10-18 22:54:37 -0400

    Added comment header

--- qualys_parser.ps1
+++ qualys_parser.ps1
@@ -1,6 +1,38 @@
-#Qualys Parser
+<#
+.SYNOPSIS
 
-# Import Qualys CSV Export File
+Author: expl01tation
+License: https://creativecommons.org/licenses/by/4.0/
+Required Dependencies: PowerShell 3.0
+Version: 1.0
+
+.DESCRIPTION
+
+Parses the supplied CSV file and generates HTML output into a file that can be emailed to a specified recipient automatically, using only native PowerShell commands.  
+
+.NOTES
+
+This script was created to perform a quick parse of the Qualys CSV format into a digestable output and automate the delivery of the generated report to the desired recipient.  
+
+This script was written in PowerShell 3
+
+Change {$_.Severity} to reflect what you want exported - as of now only Critical, High, and Medium findings are exported
+Regex for finding CVE's may need some work: ^(CVE-(1999|2\d{3})-(0\d{2}[1-9]|[1-9]\d{3,}))$
+
+Remove Garbage lines prior to processing using the below as an example:
+$a = get-content test.csv
+$a[7..($a.length -2)] > temp.csv
+$Report = Import-CSV temp.csv
+
+.LINK
+
+Twitter: https://www.twitter.com/expl01tat10n
+GitHub Repo: https://github.com/expl01tat10n
+GitHub Page: https://expl01tat10n.github.io
+
+#>
+
+# Import Qualys CSV Export File - Use the above if the CSV file is corrupted
 $Report = Import-CSV .\Qualys_Scan.csv
 $date = Get-Date
 $target = "Target Name"
